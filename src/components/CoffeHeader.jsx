f9ba4c32fc248c294496b706eb565140f364d01a
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 17:31:14 +0330

    adw

--- src/components/CoffeHeader.jsx
+++ src/components/CoffeHeader.jsx
@@ -2,18 +2,18 @@ function CoffeHeader() {
   return (
       <div className="flex justify-between items-center">
         <img
-          src="/public/assets/images/eva_menu-2-outline.png"
+          src="/assets/images/eva_menu-2-outline.png"
           alt="menu"
           className="w-8 h-8 relative top-[0.2rem] cursor-pointer"
         />
         <div className="flex space-x-4 items-center">
           <img
-            src="/public/assets/images/vector.png"
+            src="/assets/images/vector.png"
             alt="searchbar"
             className="w-6 h-6 cursor-pointer"
           />
           <img
-            src="/public/assets/images/lets-icons_basket-alt-3.png"
+            src="/assets/images/lets-icons_basket-alt-3.png"
             alt="basket"
             className="w-9 h-9 cursor-pointer"
           />
