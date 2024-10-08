9a316fb1f75758c1a6c3e6cf5e1dd16feee8b054
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 23:45:42 +0330

    asd

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Main/Home";
 
 function App() {
   return (
-    <div className="bg-body h-screen overflow-hidden scrollbar-hide w-full px-10 md:px-6 pt-5 pb-[5rem]">
+    <div className="bg-body h-screen overflow-hidden scrollbar-hide w-full px-10 md:px-6 pt-5">
       <Home/>
     </div>
   );
