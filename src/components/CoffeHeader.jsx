b180e2f33d1bb5018a0d3a4c2dd8189cc5b6c954
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 23:58:26 +0330

    Sitsh

--- src/components/CoffeHeader.jsx
+++ src/components/CoffeHeader.jsx
@@ -1,25 +1,30 @@
 function CoffeHeader() {
   return (
-      <div className="flex justify-between items-center">
+    <header className="flex justify-between items-center">
+      <button aria-label="Menu" className="focus:outline-none">
         <img
           src="/assets/images/eva_menu-2-outline.png"
-          alt="menu"
-          className="w-8 h-8 relative top-[0.2rem] cursor-pointer"
+          alt=""
+          className="w-8 h-8 cursor-pointer"
         />
-        <div className="flex space-x-4 items-center">
+      </button>
+      <div className="flex space-x-4 items-center">
+        <button aria-label="Search" className="focus:outline-none">
           <img
             src="/assets/images/Vector.png"
-            alt="searchbar"
+            alt=""
             className="w-6 h-6 cursor-pointer"
           />
+        </button>
+        <button aria-label="Basket" className="focus:outline-none">
           <img
             src="/assets/images/lets-icons_basket-alt-3.png"
-            alt="basket"
+            alt=""
             className="w-9 h-9 cursor-pointer"
           />
-        </div>
+        </button>
       </div>
-
+    </header>
   );
 }
 
