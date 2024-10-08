d16e152f6fdfb2dff02cc1298d9cce2b9c69b214
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 18:01:30 +0330

    adw

--- src/components/CoffeTitle.jsx
+++ src/components/CoffeTitle.jsx
@@ -1,7 +1,7 @@
 function CoffeTitle() {
   return (
     <div className="relative text-center mt-10">
-      <h1 className="inline-block   font-extrabold text-deep-mahogany px-4 relative text-[2.2rem] sm:w-40 lg:text-3xl">
+      <h1 className="inline-block   font-extrabold text-deep-mahogany px-4 relative text-[2.1rem] sm:w-40 lg:text-3xl">
         <span className="absolute left-[-5%] top-1 space-y-[0.4rem]">
           <span className="block w-5 h-[2px] bg-deep-mahogany transform rotate-45 mb-2"></span>
           <span className="block w-5 h-[2px] bg-deep-mahogany transform rotate-[20deg]"></span>
