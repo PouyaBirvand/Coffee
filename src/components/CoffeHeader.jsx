ba105d7238dc05ddb96c774f83040c9af1c4e17f
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 15:21:06 +0330

    adw

--- src/components/CoffeHeader.jsx
+++ src/components/CoffeHeader.jsx
@@ -1,28 +1,25 @@
-import menuIcon from '/assets/images/eva_menu-2-outline.png';
-import searchIcon from '/assets/images/vector.png';
-import basketIcon from '/assets/images/lets-icons_basket-alt-3.png';
-
 function CoffeHeader() {
   return (
-    <div className="flex justify-between items-center">
-      <img
-        src={menuIcon}
-        alt="menu"
-        className="w-8 h-8 relative top-[0.2rem] cursor-pointer"
-      />
-      <div className="flex space-x-4 items-center">
+      <div className="flex justify-between items-center">
         <img
-          src={searchIcon}
-          alt="searchbar"
-          className="w-6 h-6 cursor-pointer"
+          src="/public/assets/images/eva_menu-2-outline.png"
+          alt="menu"
+          className="w-8 h-8 relative top-[0.2rem] cursor-pointer"
         />
-        <img
-          src={basketIcon}
-          alt="basket"
-          className="w-9 h-9 cursor-pointer"
-        />
+        <div className="flex space-x-4 items-center">
+          <img
+            src="/public/assets/images/vector.png"
+            alt="searchbar"
+            className="w-6 h-6 cursor-pointer"
+          />
+          <img
+            src="/public/assets/images/lets-icons_basket-alt-3.png"
+            alt="basket"
+            className="w-9 h-9 cursor-pointer"
+          />
+        </div>
       </div>
-    </div>
+
   );
 }
 
