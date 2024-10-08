99cf80cdf4f28bd61a1a4cdfe13d80678e031a7f
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 15:06:21 +0330

    add all images

--- src/components/CoffeHeader.jsx
+++ src/components/CoffeHeader.jsx
@@ -1,25 +1,28 @@
+import menuIcon from '/assets/images/eva_menu-2-outline.png';
+import searchIcon from '/assets/images/vector.png';
+import basketIcon from '/assets/images/lets-icons_basket-alt-3.png';
+
 function CoffeHeader() {
   return (
-      <div className="flex justify-between items-center">
+    <div className="flex justify-between items-center">
+      <img
+        src={menuIcon}
+        alt="menu"
+        className="w-8 h-8 relative top-[0.2rem] cursor-pointer"
+      />
+      <div className="flex space-x-4 items-center">
         <img
-          src="/public/assets/images/eva_menu-2-outline.png"
-          alt="menu"
-          className="w-8 h-8 relative top-[0.2rem] cursor-pointer"
+          src={searchIcon}
+          alt="searchbar"
+          className="w-6 h-6 cursor-pointer"
         />
-        <div className="flex space-x-4 items-center">
-          <img
-            src="/public/assets/images/vector.png"
-            alt="searchbar"
-            className="w-6 h-6 cursor-pointer"
-          />
-          <img
-            src="/public/assets/images/lets-icons_basket-alt-3.png"
-            alt="basket"
-            className="w-9 h-9 cursor-pointer"
-          />
-        </div>
+        <img
+          src={basketIcon}
+          alt="basket"
+          className="w-9 h-9 cursor-pointer"
+        />
       </div>
-
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-function header() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
-
-export default header
