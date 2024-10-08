7f1354e9f036696300988e7d7f5d453289c39c0c
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 16:01:25 +0330

    sadw

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Home from "./pages/Main/Home";
 
 function App() {
   return (
-    <div className="bg-body min-h-screen w-full px-10 md:px-6  pt-8 flex-grow overflow-auto pb-[7rem]">
+    <div className="bg-body min-h-screen w-full px-10 md:px-6 pt-8 h-[100vh] overflow-auto pb-[7rem]">
       <Home/>
     </div>
   );
