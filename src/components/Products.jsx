323192794a99f4c26518d4a81818a519e1c9ca62
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 15:02:59 +0330

    add product images

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,28 +1,34 @@
+import iceCream from '/assets/images/ph_ice-cream-light.png';
+import drinks from '/assets/images/hugeicons_drink.png';
+import icePack from '/assets/images/hugeicons_soft-drink-01.png';
+import coffee from '/assets/images/ph_coffee-light.png';
+import pizza from '/assets/images/fluent_food-pizza-20-regular.png';
+
 function Products() {
   return (
     <div className="flex justify-between items-center shadow-md mt-[4rem] bg-warm-wood bg-opacity-60 rounded-[50rem] py-1 px-3 mx-auto w-[70%] md:w-[75%] max-w-md overflow-scroll gap-[0.5rem]">
       <img
-        src="/public/assets/images/ph_ice-cream-light.png"
+        src={iceCream}
         alt="iceCream"
         className="hover:bg-deep-mahogany p-2 rounded-full transition duration-300 cursor-pointer bg-custom-2 bg-opacity-50 backdrop-filter backdrop-blur-sm"
       />
       <img
-        src="/public/assets/images/hugeicons_drink.png"
+        src={drinks}
         alt="Drinks"
         className="hover:bg-deep-mahogany p-2 rounded-full transition duration-300 cursor-pointer bg-custom-2 bg-opacity-50 backdrop-filter backdrop-blur-sm"
       />
       <img
-        src="/public/assets/images/hugeicons_soft-drink-01.png"
+        src={icePack}
         alt="icePack"
         className="hover:bg-deep-mahogany p-2 rounded-full transition duration-300 cursor-pointer bg-custom-2 bg-opacity-50 backdrop-filter backdrop-blur-sm"
       />
       <img
-        src="/public/assets/images/ph_coffee-light.png"
+        src={coffee}
         alt="Coffes"
         className="hover:bg-deep-mahogany p-2  rounded-full transition duration-300 bg-deep-mahogany cursor-pointer"
       />
       <img
-        src="/public/assets/images/fluent_food-pizza-20-regular.png"
+        src={pizza}
         alt="Pizzas"
         className="hover:bg-deep-mahogany p-2 rounded-full transition duration-300 cursor-pointer bg-custom-2 bg-opacity-50 backdrop-filter backdrop-blur-sm"
       />
