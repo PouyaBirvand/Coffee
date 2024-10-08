72d4d7ef37dc5aa9fd4acc46ff5bb913f258af23
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-08 15:38:57 +0330

    das

--- src/components/BottomNavigation.jsx
+++ src/components/BottomNavigation.jsx
@@ -3,7 +3,10 @@ function BottomNavigation() {
     <div className="fixed bottom-0 left-0 right-0 bg-dark-cocoa z-[999]">
       <div className="flex justify-around items-center h-14 relative">
         <div className="absolute -top-6 left-1/2 transform -translate-x-1/2">
-          <div className="bg-dark-cocoa p-2 rounded-full shadow-lg border border-white">        
+          <div className="bg-dark-cocoa p-2 rounded-full shadow-lg border border-white">
+            <div className="bg-dark-cocoa w-[5.5rem] h-[3rem] -z-20 absolute -top-2 left-1/2 transform -translate-x-1/2 rounded-t-full"></div>
+        
+
             <img
               src="/public/assets/images/ion_restaurant-outline.png"
               alt="Restaurant icon"
