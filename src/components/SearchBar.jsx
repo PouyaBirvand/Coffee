3959eb874235bacd8d36f27c73a9a58dcf5d6b8a
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-09 21:56:34 +0330

    sdwa

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -10,11 +10,11 @@ function SearchBar({ onClose }) {
       transition={{ duration: 0.3 }}
       className="flex items-center bg-soft-cream px-4 py-1 mt-2 w-full rounded-full shadow-md relative z-30"
     >
-      <form className="flex-grow flex items-center">
+      <form className="flex-grow flex items-center px-1">
         <input
           type="text"
           placeholder="Search anything you want ..."
-          className="w-full py-2 focus:outline-none bg-soft-cream placeholder:text-dark-cocoa"
+          className="w-full py-2 relative top-[0.1rem] focus:outline-none bg-soft-cream placeholder:text-dark-cocoa"
         />
         <button type="submit" className="focus:outline-none ml-2">
           <img src="/assets/images/searchbar.png" alt="Search" />
