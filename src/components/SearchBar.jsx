c84daeae7e80342fa650710b1bd67bc292fbda5f
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-09 22:02:21 +0330

    asdws

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,7 +1,16 @@
+import { useEffect, useRef } from 'react';
 import { motion } from 'framer-motion';
 
 // eslint-disable-next-line react/prop-types
 function SearchBar({ onClose }) {
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
     <motion.div
       initial={{ opacity: 0, y: -20 }}
@@ -10,13 +19,14 @@ function SearchBar({ onClose }) {
       transition={{ duration: 0.3 }}
       className="flex items-center bg-soft-cream px-4 py-1 mt-2 w-full rounded-full shadow-md relative z-30"
     >
-      <form className="flex-grow flex items-center px-1">
+      <form className="flex-grow flex items-center px-2  relative top-[0.2rem]">
         <input
+          ref={inputRef}
           type="text"
           placeholder="Search anything you want ..."
-          className="w-full py-2 relative top-[0.1rem] focus:outline-none bg-soft-cream placeholder:text-dark-cocoa"
+          className="w-full py-2 focus:outline-none bg-soft-cream placeholder:text-dark-cocoa"
         />
-        <button type="submit" className="focus:outline-none ml-2">
+        <button type="submit" className="focus:outline-none ml-2 mb-1">
           <img src="/assets/images/searchbar.png" alt="Search" />
         </button>
       </form>
