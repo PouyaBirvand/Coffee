06cef128391a03577558a50cd07e2d995087cba3
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-09 21:00:11 +0330

    chaw

--- src/pages/Main/Home.jsx
+++ src/pages/Main/Home.jsx
@@ -1,8 +1,8 @@
-import Header from "/../components/Header";
-import ProductTitle from "/../components/ProductTitle";
-import CartItems from "/../components/CartItems";
-import BottomNavigation from "/../components/BottomNavigation";
-import Categories from "/../components/Categories";
+import Header from "../../components/Header";
+import ProductTitle from "../../components/ProductTitle";
+import CartItems from "../../components/CartItems";
+import BottomNavigation from "../../components/BottomNavigation";
+import Categories from "../../components/Categories.jsx";
 
 
 function Home() {
