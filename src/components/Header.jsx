ee59784ad31d66cb79593fee0f385ef9d76a16ef
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-12 00:47:48 +0330

    lordicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Menu from "./Menu";
 import SearchBar from "./SearchBar";
-import LordIcon from "c:/Users/resam/Desktop/Fast-Food-Shop/src/components/Lordicon";
+import LordIcon from "./Lordicon.jsx";
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
