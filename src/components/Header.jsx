7fa34cb0afb9a2af84ae5057735407a16ff2a727
PouyaBirvand <pooyabirvand@gmail.com>
2024-10-18 19:59:56 +0330

    fix other bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from "react";
-import { AnimatePresence, motion } from "framer-motion";
+import { AnimatePresence, motion, transform } from "framer-motion";
 import Menu from "./Menu";
 import SearchBar from "./SearchBar";
 import { useAppContext } from "../context/AppContext";
@@ -63,7 +63,7 @@ function Header() {
               trigger="loop"
               delay="0"
               colors="primary:#412f26"
-              style={{ width: "35px", height: "35px" }}
+              style={{ width: "35px", height: "35px", transform: "rotate(180deg)" }}
             />
           </button>
         )}
