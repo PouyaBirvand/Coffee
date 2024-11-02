84224340f34aaa4a3802ffc58a0ee420b48ecdbc
PouyaBirvand <pooyabirvand@gmail.com>
2024-11-03 02:08:49 +0330

    kossher ediiiiiiiiiiiiiiiiit

--- src/components/usePrdouct.js
+++ src/components/usePrdouct.js
@@ -206,6 +206,8 @@ const initialItems = [
     }
   ];
 
+  
+
   const getProductsByCategory = async (category) => {
     await new Promise(resolve => setTimeout(resolve, 300)) // شبیه‌سازی تاخیر شبکه
     const filteredItems = initialItems.filter(item => item.category === category)
@@ -215,6 +217,8 @@ const initialItems = [
   export function useProducts(category) {
     return useQuery({
       queryKey: ['products', category],
-      queryFn: () => getProductsByCategory(category)
-    })
+      queryFn: () => getProductsByCategory(category),
+      staleTime: 1000 * 60 * 5, // Cache for 5 minutes
+      cacheTime: 1000 * 60 * 30, // Keep in cache for 30 minutes
+    });
   }
